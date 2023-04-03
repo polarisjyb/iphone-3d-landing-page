90800771544ad459786884785beddb67148412a2
polarisjyb <polarislsj@gmail.com>
2023-04-03 14:45:02 +0900

    createGlobalStyle() 문법 실수로 수정

--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 import fontLight from "'../assets/fonts/SourceSansPro-Light.ttf';"
 import fontRegular from "'../assets/fonts/SourceSansPro-Regular.ttf';"
 
-export const createGlobalStyle = `
+export const GlobalStyle = createGlobalStyle `
   *, *::before, *::after {
     margin: 0;
     padding: 0;
