a46fe0dea6beb88e4e56a04e5446d51eace13bc4
polarisjyb <polarislsj@gmail.com>
2023-04-03 14:56:46 +0900

    add Quotation sections & browser render test

---
+++ src/sections/Quotation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+`
+
+const Quotation = () => {
+  return (
+    <Section>Quotation</Section>
+  );
+};
+
+export default Quotation;
