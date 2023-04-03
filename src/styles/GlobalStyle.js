7ef2e1156d746faa925b6b53cdc76e346724f576
polarisjyb <polarislsj@gmail.com>
2023-04-03 16:16:30 +0900

    font-face 웹폰트 & body font-family 적용

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,4 +7,25 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+
+  @font-face {
+    font-family: 'Source Sans Pro light';
+    src: local('Source Sans Pro light'), url(${fontLight}) format("truetype") ;
+    font-display:swap;
+    font-style: normal;
+  }
+  
+  @font-face {
+    font-family: 'Source Sans Pro';
+    src: local('Source Sans Pro'), url(${fontRegular}) format("truetype") ;
+    font-display:swap;
+    font-style: normal;
+  }
+
+  body{
+    font-family: "Source Sans Pro", sans-serif;
+    overflow-x: hidden;
+  }
+
+  
 `
