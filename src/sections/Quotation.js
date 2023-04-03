c4f9ae1cef3df76b1fdc585024c3aeb4eccd92aa
polarisjyb <polarislsj@gmail.com>
2023-04-03 16:13:19 +0900

    Quotation - TextContainer 스타일 컴포넌트 생성

--- src/sections/Quotation.js
+++ src/sections/Quotation.js
@@ -12,17 +12,27 @@ const Section = styled.section`
 `
 
 const TextContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const Quotation = () => {
   return (
     <Section>
-      Sometimes Life is going to hit you in the head with a brick. Don't lose faith.
-      <br></br>
-      Let's go invent tomorrow rather than worrying about what happened yesterday.
+      <TextContainer>
+        <span>
+          Sometimes Life is going to hit you in the head with a brick. Don't lose faith.
+        </span>
+        <span>
+          Let's go invent tomorrow rather than worrying about what happened yesterday.
+        </span>
+      </TextContainer>
     </Section>
-
   );
 };
 
