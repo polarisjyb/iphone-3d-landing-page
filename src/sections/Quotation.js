532bf38c492adc3870a840efe54605117f8b57f8
polarisjyb <polarislsj@gmail.com>
2023-04-03 15:00:02 +0900

    인용 문구 스타일 수정 - display: flex

--- src/sections/Quotation.js
+++ src/sections/Quotation.js
@@ -2,10 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const Section = styled.section`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   position: relative;
 
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const Quotation = () => {
