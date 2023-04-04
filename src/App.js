4fdc53643a2034d3b6def1a0e2387ae65b7fc278
polarisjyb <polarislsj@gmail.com>
2023-04-04 17:30:23 +0900

    App.js 에 InkSection 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { GlobalStyle } from "./styles/GlobalStyle";
 import Quotation from "./sections/Quotation";
+import InkSection from "./sections/Ink";
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Quotation />
+      <InkSection />
     </div>
   );
 }
