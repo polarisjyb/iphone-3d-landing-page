8f6eda889dce50b99a095c543251430951a1066d
polarisjyb <polarislsj@gmail.com>
2023-04-05 18:44:38 +0900

    ModelContainer - styled z-index: 1 & Suspense fallback = null

--- src/sections/PhoneModel.js
+++ src/sections/PhoneModel.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
+import { Suspense } from "react";
+
 import Model from "../assets/3D-Model/Scene";
 
 const ModelContainer = styled.div`
@@ -10,7 +12,7 @@ const ModelContainer = styled.div`
 
   position: fixed;
   top: 0;
-  z-index: 2;
+  z-index: 1;
   background-color: transparent;
 `
 
@@ -20,7 +22,9 @@ const PhoneModel = () => {
       <Canvas camera={{fov: 20}}>
         <ambientLight intensity={1.5}/>
         <directionalLight intensity={0.5} />
-          <Model />
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
           <Environment preset="night" />
       </Canvas>
     </ModelContainer>
