dc3beaeec76b9e104cac836c9f0d6debb5a6b993
polarisjyb <polarislsj@gmail.com>
2023-04-06 15:53:08 +0900

    ProcessorSection 추가

---
+++ src/sections/ProcessorSection.js
@@ -0,0 +1,83 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import a15 from "../assets/Images/A15-Bionic.jpg";
+
+const Section = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+
+  display: flex;
+  justify-content: center;
+  align-items: cetner;
+
+  background-color: var(--dark);
+  color: var(--white);
+`
+
+const Title = styled.div`
+  width: 100%;
+  position: absolute;
+  
+  display: flex;
+  justify-content: center;
+  align-self: flex-end;
+
+  font-size: var(--fontBig);
+  font-family: var(--fontR);
+  z-index: 1;
+
+  background-image: linear-gradient(90deg, var(--gradient));
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+const Glow = keyframes`
+  0%{
+    box-shadow: 1px 1px 10px var(--white);
+  }
+  50%{
+    box-shadow: 2px 2px 25px var(--white);
+  }
+  100%{
+    box-shadow: 1px 1px 10px var(--white);
+  }
+`
+const Processor = styled.div`
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  // position: absolute;
+  // top: 50%;
+  // left: 50%;
+  // transform: translate(-50%, -50%);
+  // animation: ${Glow} 3s ease infinite;
+  // padding: 0.5rem;
+
+  img {
+    width: 100%;
+    height: auto;
+    animation: ${Glow} 3s ease infinite;
+    padding: 0.5rem;
+  }
+`
+
+
+
+const ProcessorSection = () => {
+  return (
+    <Section>
+      <Title>
+        Fastest Processor
+      </Title>
+      <Processor>
+        <img src={a15} alt="A15 processor" />
+      </Processor>
+    </Section>
+  );
+};
+
+export default ProcessorSection;
