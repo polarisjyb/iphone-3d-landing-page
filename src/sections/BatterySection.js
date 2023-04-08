ef0e4c375132ff8e8200b40aa7d300529c91ab02
polarisjyb <polarislsj@gmail.com>
2023-04-08 16:34:38 +0900

    BatterySection Title styled 변경

--- src/sections/BatterySection.js
+++ src/sections/BatterySection.js
@@ -12,19 +12,11 @@ const Section = styled.section`
   background-color: var(--white);
 `;
 
-const Title = styled.div`
-  width: 100%;
-
-  padding-right: 3em;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;  
-
-  // position: absolute;
-  // top: 50%;
-  // left: 50%;
-  // transform: translate(-55%, -50%);
+const Title = styled.h1`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-55%, -50%);
 
   text-transform: capitalize;
   font-size: var(--fontBig);
@@ -32,10 +24,11 @@ const Title = styled.div`
   z-index: 1;
 `;
 
+
 const BatterySection = () => {
   return(
     <Section>
-      <Title>Go all day with <br /> single charge...</Title>
+      <Title>Go all day with single charge...</Title>
     </Section>
   );
 };
