5d76cfeab57696e0155270aab983a871685d5fbd
polarisjyb <polarislsj@gmail.com>
2023-04-28 17:48:18 +0900

    Scene - PhoneModel 파일 모델의 position 이동 애니메이션 설정

--- src/assets/3D-Model/Scene.jsx
+++ src/assets/3D-Model/Scene.jsx
@@ -7,11 +7,36 @@ Source: https://sketchfab.com/3d-models/apple-iphone-13-pro-max-4328dea00e47497d
 Title: Apple iPhone 13 Pro Max
 */
 
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import React, { useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { useGLTF } from '@react-three/drei';
+import { useThree } from '@react-three/fiber';
 
 export default function Model(props) {
-  const { nodes, materials } = useGLTF('/scene.gltf')
+
+  const { nodes, materials } = useGLTF('/scene.gltf');
+
+  let camera = useThree(state => state.camera);
+
+  useLayoutEffect(() => {
+
+    let timeline1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#phone-model",
+        start: "top top",
+        end: "bottom+=500 bottom",
+        markers: true,
+      },
+    });
+
+    timeline1.fromTo(camera.position, {y:2}, {y:0})
+
+
+    // PerspectiveCamera 객체 확인
+    // console.log(camera);
+
+  }, []);
+
   return (
     <group {...props} dispose={null}>
       <group scale={0.01}>
@@ -48,7 +73,7 @@ export default function Model(props) {
         </group>
       </group>
     </group>
-  )
-}
+  );
+};
 
-useGLTF.preload('/scene.gltf')
+useGLTF.preload('/scene.gltf');
