fc56eabebef589458192a442c1cb28c3c7bb9520
polarisjyb <polarislsj@gmail.com>
2023-05-05 22:49:09 +0900

    style 조정 및 gsap 애니메이션 적용

--- src/sections/CameraSection.js
+++ src/sections/CameraSection.js
@@ -1,16 +1,19 @@
+import gsap from "gsap";
 import React from "react";
+import { useLayoutEffect } from "react";
+import { useRef } from "react";
 import styled from "styled-components";
 import v1 from "../assets/video/Scuba Diving - 699.mp4";
 import v2 from "../assets/video/Skate - 49791.mp4";
 
-const Section = styled.div`
+const Section = styled.section`
   width: 100vw;
-  minheight: 100vh;
+  min-height: 100vh;
   position: relative;
   z-index: 1;
-
   background-color: var(--white);
-`
+  overflow: hidden;
+`;
 
 const V1 = styled.video`
   position: absolute;
@@ -21,7 +24,7 @@ const V1 = styled.video`
   object-fit: cover;
   object-position: bottom;
   z-index: 2;
-`
+`;
 
 const V2 = styled.video`
   position: absolute;
@@ -30,8 +33,8 @@ const V2 = styled.video`
   width: 60%;
   height: auto;
 
-  z-index: 2;
-`
+  z-index: 1;
+`;
 
 const TitleContainer = styled.div`
   width: 50%;
@@ -44,27 +47,88 @@ const TitleContainer = styled.div`
   top: 0;
   right: 0;
 
-  z-index: 2;
-
   & > *:nth-child(2) {
     margin-left: 6rem;
   }
   & > *:nth-child(3) {
     margin-left: 12rem;
   }
-`
+`;
 
 const Title = styled.h1`
   font-size: var(--fontBig);
+  z-index: 5;
   text-transform: capitalize;
-`
+`;
 
 const CameraSection = () => {
-  return(
-    <Section>
-      <V1 src={v1} type="video/mp4" autoPlay muted loop />
-      <V2 src={v2} type="video/mp4" autoPlay muted loop />
-      <TitleContainer>
+  const sectionRef = useRef(null);
+
+  const videoRef1 = useRef(null);
+  const videoRef2 = useRef(null);
+  const titleRef = useRef(null);
+
+  let elements = gsap.utils.selector(titleRef);
+
+  useLayoutEffect(() => {
+    const Elem = sectionRef.current;
+    const video1Elem = videoRef1.current;
+    const video2Elem = videoRef2.current;
+
+    // pin the section
+    gsap.to(Elem, {
+      scrollTrigger: {
+        trigger: Elem,
+        start: "top top",
+        end: `bottom+=500 bottom`,
+        scrub: 1,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    let t2 = gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: Elem,
+          start: "top top",
+          end: `bottom+=500 bottom`,
+          scrub: 1,
+        },
+      })
+      .to(video1Elem, { scale: 0.3 }, "key1")
+      .to(video2Elem, { scale: 0.6 }, "key1");
+
+    elements("h1").forEach((el) =>
+      t2.fromTo(
+        el,
+        {
+          scrollTrigger: {
+            trigger: el,
+            start: "top top",
+            end: `bottom bottom`,
+            scrub: 1,
+          },
+          x: 100,
+          opacity: 0,
+        },
+        {
+          x: 0,
+          opacity: 1,
+        }
+      )
+    );
+
+    return () => {
+      if (t2) t2.kill();
+    };
+  }, []);
+
+  return (
+    <Section ref={sectionRef}>
+      <V1 ref={videoRef1} src={v1} type="video/mp4" autoPlay muted loop />
+      <V2 ref={videoRef2} src={v2} type="video/mp4" autoPlay muted loop />
+      <TitleContainer ref={titleRef}>
         <Title>test1</Title>
         <Title>test2</Title>
         <Title>test3</Title>
